e55b27dd9ad3394920a3936a237484c2daf5540d
TassioS <44905214+tassio105@users.noreply.github.com>
2020-12-19 09:02:56 -0500

    update on script.js code
    use of let and if statement to declare and pass variables/strings

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log(40 + 8 + 23 - 10);
+let firstName = "Tassio";
+console.log(firstName);
+
+let country = 'Country';
+let continent = 'Continent';
+let population = 'Population';
+
+if (country === 'Country' && continent === 'Continent' && population === 'Population')
+    console.log("Brazil, South America, 212 Billion people");
+
+//if (country === 'Country') console.log("Brazil");
+//if (continent === 'Continent') console.log("South America");
+//if (population === 'Population') console.log("212.6 Billion people");
