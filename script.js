b39ec94aef9c5792fe42d4aed6e130b4a48dfa39
TassioS <44905214+tassio105@users.noreply.github.com>
2020-12-22 10:42:29 -0500

    Lecture 2
    data types and discover type of variables after using "typeof"

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 console.log(40 + 8 + 23 - 10);
 let firstName = "Tassio";
 console.log(firstName);
@@ -17,3 +18,43 @@ if (country === 'Country' && continent === 'Continent' && population === 'Popula
 //if (country === 'Country') console.log("Brazil");
 //if (continent === 'Continent') console.log("South America");
 //if (population === 'Population') console.log("212.6 Billion people");
+*/
+
+// Variable types
+//true;
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Tassio');
+
+javascriptIsFun = 'Yes';
+console.log(typeof javascriptIsFun);
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+// Exercise - 2
+let country = 'Country';
+let population = 'Population';
+
+let isIsland;
+let language;
+isIsland = true;
+if (isIsland === true) {
+    isIsland = "Fernando de Noronha";
+}
+language = 'Portuguese';
+
+console.log(isIsland, ",", population, ",", country, ",", language);
