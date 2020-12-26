f446c43454599246a5e67a0707ea56998b6a1b2d
TassioS <44905214+tassio105@users.noreply.github.com>
2020-12-26 12:20:16 -0500

    Operators precedence and Code Challenge
    Reviewed the which operation will start first according to the operator precedence => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
    
    Also worked on the challenge in 2 different ways + copied the way it was solved on the class.

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ console.log(isIsland, ",", population, ",", country, ",", language);
 */
 
 // Lecture 3 - Cons and var
-
+/*
 let age = 30;
 age = 31;
 
@@ -77,7 +77,149 @@ job = 'teacher'
 
 lastName = 'Andrade';
 console.log(lastName);
-
+*/
 
 //Lecture 4 - Operatiors 
+/*
+const now = 2037; // used to declare a variable that will not be changed
+const ageTassio = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageTassio, ageSarah);
+
+console.log(ageTassio * 2, ageTassio / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Tassio';
+const lastName = 'Andrade';
+console.log(firstName + ' ' + lastName); //concatination using '+' sign
+
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x +1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison operators
+console.log(ageTassio > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+//Lecture 5 - operators precedence
+/*
+const now = 2037; // used to declare a variable that will not be changed
+const ageTassio = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018); // based on mdn operator precedence - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // subtraction will start first due to the operator precedence and then the assignment '=' will start next
+console.log(x, y);
+
+const averageAge = (ageTassio + ageSarah) / 2;
+console.log(ageTassio, ageSarah, averageAge);
+*/
+
+//Lecture 6 - code challenge
+
+/*Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+
+Test data:
+Test Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+Test Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+*/
+
+// TEST 1
+//const BMI = mass / height ** 2; //= mass / (height * height);
+let markBMI;
+let johnBMI;
+
+const markHeight = 1.69;
+const johnHeight = 1.95;
+
+let markWeight = 78;
+let johnWeight = 92;
+
+markBMI = markWeight / markHeight ** 2;
+johnBMI = johnWeight / johnHeight ** 2;
+
+console.log('Mark BMI is equal to: ' + markBMI + ' and John BMI is equal to ' + johnBMI);
+
+//TEST 2
+
+let markBMI2;
+let johnBMI2;
+
+const markHeight2 = 1.88;
+const johnHeight2 = 1.76;
+
+let markWeight2 = 95;
+let johnWeight2 = 85;
+
+markBMI2 = markWeight2 / markHeight2 ** 2;
+johnBMI2 = johnWeight2 / johnHeight2 ** 2;
+
+console.log('Mark BMI is equal to: ' + markBMI2 + ' and John BMI is equal to ' + johnBMI2);
+
+let markHigherBMI = markBMI > johnBMI;
+let johnHigherBMI = johnBMI > markBMI;
+let markHigherBMI2 = markBMI2 > johnBMI2;
+let johnHigherBMI2 = johnBMI2 > markBMI2;
+
+console.log(markHigherBMI, johnHigherBMI);
+console.log(markHigherBMI2, johnHigherBMI2);
+
+// other ways to write the challenge
+mark_Weight = 70;
+mark_Height = 1.70;
+john_Weight = 75;
+john_Height = 1.80;
+
+let mark_BMI = mark_Weight / mark_Height ** 2;
+let john_BMI = john_Weight / john_Height ** 2;
+
+let comparison_MarkAndJohn;
+
+if (mark_BMI > john_BMI) {
+    comparison_MarkAndJohn = mark_BMI;
+    console.log('Mark BMI is ' + mark_BMI + ' and John BMI is ' + john_BMI + '. Mark has a higher BMI than John.', comparison_MarkAndJohn);
+}
+else {
+    comparison_MarkAndJohn = john_BMI;
+    console.log('Mark BMI is ' + mark_BMI + ' and John BMI is ' + john_BMI + '. John has a higher BMI than Mark.', comparison_MarkAndJohn);
+}
+
+// Resolution of the challenge
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+const markHigher_BMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigher_BMI);
 
